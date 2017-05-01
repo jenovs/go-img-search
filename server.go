cc6b8db48a4a007ac7c1221e92308e104f1ccfb2
Viktors Jenovs <jviktors@gmail.com>
2017-05-01 07:58:11 +0300

    Inital commit

---
+++ server.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func getPort() string {
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "3000"
+	}
+	return ":" + port
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	os.Setenv("TEST", "this is a test")
+	fmt.Println(os.Getenv("TEST"))
+}
+
+func faviconHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./favicon.ico")
+}
+
+func main() {
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/favicon.ico", faviconHandler)
+	log.Fatal(http.ListenAndServe(getPort(), nil))
+}
